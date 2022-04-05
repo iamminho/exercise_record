22d8ffb609f016433589f9a9c43ca4d0a00f9320
iamminho <minho100227@kakao.com>
2022-04-05 12:43:04 +0900

    useReducer를 이용해 상태관리 최적화

--- src/App.js
+++ src/App.js
@@ -1,38 +1,55 @@
-import {useEffect, useCallback, useRef, useState} from "react";
+import {useEffect, useCallback, useRef, useReducer} from "react";
 import './App.css';
 import ExerciseEdit from './ExerciseEdit';
 import ExerciseList from './ExerciseList';
 import LifeCycle from './LifeCyle';
 
+
+const reducer = (state, action) => {
+  switch ((action.type)) {
+    case "CREATE": {
+      const created_date = new Date().getTime();
+      const newItem = {
+        ...action.data,
+        created_date,
+      };
+      return [newItem, ...state];
+    }
+    case "REMOVE": {
+      return state.filter((it) => it.id !== action.targetId);
+    }
+    case "EDIT": {
+      return state.map((it) =>
+        it.id === action.targetId ? 
+        { ...it, content: action.newContent } : it
+      );
+    }
+    default:
+      return state;
+  }
+};
+
 function App() {
 
-  const [data,setData] = useState([]);
+  const [data, dispatch] = useReducer(reducer, []);
 
   const dataId = useRef(0);
 
-  const onCreate = useCallback((author,content) => {
-    const created_date = new Date().getTime();
-    const newItem = {
-      author,
-      content,
-      created_date,      
-      id : dataId.current
-    }
+  const onCreate = useCallback((author, content) => {
+    dispatch({
+      type: `CREATE`, 
+      data: { author, content, id: dataId.current } 
+    });
+    // 
     dataId.current += 1;
-    setData((data) => [newItem, ...data])
-  }, [] );
+  }, []);
 
   const onDelete = useCallback((targetId) => {
-    console.log(`${targetId}가 삭제되었습니다.`);
-    setData((data) => data.filter((it) => it.id !== targetId));
+    dispatch({ type: "REMOVE", targetId });
   }, []);
 
   const onEdit = useCallback((targetId, newContent) => {
-    setData((data) =>
-      data.map((it) =>
-        it.id === targetId ? { ...it, content: newContent } : it
-      )
-    );
+    dispatch({ type: "EDIT", targetId, newContent });
   }, []);
 
   return (
